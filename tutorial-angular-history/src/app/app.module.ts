6ee3f3ddab95544a95e88b1de4170f8e78ab5296
Fabien GAUTREAULT <fabien.gautreault@gmail.com>
2021-04-02 19:29:23 +0200

    Core Module

--- tutorial-angular-history/src/app/app.module.ts
+++ tutorial-angular-history/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
