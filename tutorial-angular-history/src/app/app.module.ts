54e174b812715ffaf66d69ad45914d2218cb5a22
Fabien GAUTREAULT <fabien.gautreault@gmail.com>
2021-04-03 08:15:59 +0200

    Footer

--- tutorial-angular-history/src/app/app.module.ts
+++ tutorial-angular-history/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './/app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
